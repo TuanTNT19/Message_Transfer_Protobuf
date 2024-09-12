af3e0787a9b658022e29655aa4bfc295e9192a1d
TuanTNT19 <tuantnt08@gmail.com>
2024-09-12 10:01:10 +0700

    update

--- nvidia_code/myQueue.h
+++ nvidia_code/myQueue.h
@@ -9,55 +9,52 @@ using namespace std;
 
 template <typename... Types>
 class myQueue {
-private:
+    private:
     int front, rear, size;
     unsigned capacity;
-    std::variant<Types...>* array;
+    variant <Types...> *array;
 
-public:
-    // Constructor để khởi tạo Queue với kích thước cho trước
+    public:
     myQueue(unsigned capacity)
         : capacity(capacity), front(0), size(0), rear(capacity - 1) {
         array = new std::variant<Types...>[this->capacity];
     }
 
-    // Destructor để giải phóng bộ nhớ khi đối tượng Queue bị hủy
     ~myQueue() {
         delete[] array;
     }
 
-    // Kiểm tra Queue có đầy hay không
     bool isFull() const {
         return (size == capacity);
     }
 
-    // Kiểm tra Queue có rỗng hay không
     bool isEmpty() const {
         return (size == 0);
     }
 
-    // Thêm phần tử vào đầu Queue ( cuối mang)
-    void push(const std::variant<Types...>& item) {
+    void push(const variant<Types...>& item) {
         if (isFull()) {
-            throw overflow_error("Queue is full");
+            cerr << "Queue is full" << endl;
+            return;
         }
+
         rear = (rear + 1) % capacity;
         array[rear] = item;
         size = size + 1;
     }
 
-    // Lấy phần tử từ cuối queue ( đầu mảng)
-    std::variant<Types...> pop() {
+    variant <Types...> pop() {
         if (isEmpty()) {
-            throw out_of_range("Queue is empty");
+            cerr << "Queue is empty" << endl;
+            return ;
         }
-        std::variant<Types...> item = array[front];
+
+        auto item = array[front];  
         front = (front + 1) % capacity;
         size = size - 1;
         return item;
     }
 
-    // Lấy kích thước của Queue
     int getSize() const {
         return size;
     }
